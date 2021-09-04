680384d00e9d0840e0b8888b6014afafe7fbbf29
chanhyun <stephano2037@gmail.com>
2021-09-05 05:30:46 +0900

    re #4 create first genesisblock

--- main.go
+++ main.go
@@ -2,33 +2,63 @@
 21.09.04 시작
 https://nomadcoders.co/nomadcoin/lectures/2939
 
- GOPATH=D:\go_project
- GOPROXY=https://proxy.golang.org,direct
- GOROOT=C:\Program Files\Go
- GOBIN=
+
 
 노마드 코인 go 언어로 제작해보기
 - 단, 실제 코인이 아닌, 어떤 식으로 "동작"하는지 해당 개념을 구현하는 프로젝트임
 - making "Useless Coin "
 
+
 */
 
-//go 함수는 대문자로 시작한다
 package main
 
 import (
-	"cryptocoin/person"
+	"crypto/sha256"
 	"fmt"
 )
 
+/*
+block chain structure
+
+
+hash -> one way function (not duplix)
+"hello" -> h_fn(x) -> "wjiofjewoijfowejof"
+"hello1" -> h_fn(x) -> "wszcvzxcvbbnnnnnn!!"
+can't get original value by reverse
+*/
+
+//block 1번의 hash 는 block 1번의 data + 이전 block의 hash 로 계산
+//block 1번이 변경되면 block 3번도 변경된다 그래야 의미있다
+//common block chain's way of hash is using [SHA256] Algorithm
+//Go language has already have this algorithm
+//hash 가 원하는 것은 불변하는 값
+/*
+	b1
+		b1hash = (data + "")
+			or
+		b1hash. = (data + "x")
+	b2
+		b2hash.. = (data + b1hash)
+	b3
+		b3hash... = (data + b2hash)
+*/
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
 func main() {
-	stephano := person.Person{}
+	//블록체인의 시작을 보통 genesisBlock 이라 명명하는듯
+	genesisBlock := block{"Genesis Block", "", ""}
+	//get slice array byte
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	//fmt.Printf("%x,%T\n", hash, hash)
+	//fmt.Println(len(hash))
 
-	//copy
-	stephano.SetDetails("stephano", 12)
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+	fmt.Printf("%v,%T\n", genesisBlock.hash, genesisBlock.hash)
 
-	//it didn't show the previous assignment data
-	//fmt.Println(stephano)
-
-	fmt.Println(stephano.GetName())
 }
