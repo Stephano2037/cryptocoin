674649ad3f7c353998d011164c24030b28291f42
chanhyun <stephano2037@gmail.com>
2021-09-07 04:58:41 +0900

    re #33 make blockchain functions - add blockchain -- using sha256 algorithm for making hash - print list of blockchain

--- main.go
+++ main.go
@@ -49,16 +49,38 @@ type block struct {
 	prevHash string
 }
 
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	//find previous hash
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("hash: %s\n", block.hash)
+		fmt.Printf("prev hash: %s\n", block.prevHash)
+	}
+}
+
 func main() {
-	//블록체인의 시작을 보통 genesisBlock 이라 명명하는듯
-	genesisBlock := block{"Genesis Block", "", ""}
-	//get slice array byte
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	//fmt.Printf("%x,%T\n", hash, hash)
-	//fmt.Println(len(hash))
-
-	hexHash := fmt.Sprintf("%x", hash)
-	genesisBlock.hash = hexHash
-	fmt.Printf("%v,%T\n", genesisBlock.hash, genesisBlock.hash)
-
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.listBlocks()
 }
