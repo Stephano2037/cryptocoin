fbffde22735839e068be1d6be9a8e547d24f70f2
chanhyun <stephano2037@gmail.com>
2021-09-07 05:08:50 +0900

    re #34 make singleton

--- main.go
+++ main.go
@@ -14,8 +14,7 @@ https://nomadcoders.co/nomadcoin/lectures/2939
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
+	"cryptocoin/blockchain"
 )
 
 /*
@@ -43,44 +42,8 @@ can't get original value by reverse
 	b3
 		b3hash... = (data + b2hash)
 */
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	//find previous hash
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-
-}
-
-func (b *blockchain) listBlocks() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("hash: %s\n", block.hash)
-		fmt.Printf("prev hash: %s\n", block.prevHash)
-	}
-}
 
 func main() {
-	chain := blockchain{}
-	chain.addBlock("Genesis Block")
-	chain.addBlock("Second Block")
-	chain.addBlock("Third Block")
-	chain.listBlocks()
+	chain := blockchain.GetBlockchain()
+
 }
